47835887b12c9f2fc0634ef6d6e34fecb7a3e2fb
tomogoapp <chris.tomogo@gmail.com>
2018-11-27 10:40:37 -0600

    Controlladores de nueva bicicleta

--- app/NuevaBicicleta.php
+++ app/NuevaBicicleta.php
@@ -10,6 +10,8 @@ class NuevaBicicleta extends Model
     * The table associated with the model.
     * @var string   
     */
+
+    // --- Cambios del 27/Noviembre/2018
     //protected $connection = '';
     protected $table = 'ag_bicicletas';
     public $timestamps = false;
