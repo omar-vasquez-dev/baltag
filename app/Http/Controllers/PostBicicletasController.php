701f1c0ae115fc98d5351767a6569f3a51323391
tomogoapp <chris.tomogo@gmail.com>
2018-11-27 11:02:49 -0600

    Controlladores con Vue js

--- app/Http/Controllers/PostBicicletasController.php
+++ app/Http/Controllers/PostBicicletasController.php
@@ -7,12 +7,7 @@ use Illuminate\Http\Request;
 
 class PostBicicletasController extends Controller
 {
-    //
-    public function postBicicleta(Request $request)
-    {
-        $NuevaBicicleta = new NuevaBicicleta;
-        $NuevaBicicleta->descripcion = $request->descripcion;
 
-        $NuevaBicicleta->save();
-    }
+    
+
 }
